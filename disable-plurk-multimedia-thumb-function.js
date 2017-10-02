83a98e4dee7557b6159c49473d4bb99a1e4ce2f1
Gea-Suan Lin <gslin@gslin.org>
2017-10-03 05:50:15 +0800

    Import 0.2.

---
+++ disable-plurk-multimedia-thumb-function.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Disable Plurk multimedia thumb functuion
+// @namespace    https://blog.gslin.org/plurk
+// @version      0.2
+// @description  Disable Plurk multimedia thumb function
+// @author       Gea-Suan Lin <darkkiller@gmail.com>
+// @match        *://www.plurk.com/*
+// @grant        none
+// ==/UserScript==
+
+(function(){
+    document.getElementsByTagName('body')[0].addEventListener('click', function(e){
+        for (var el = e.target; el; el = el.parentElement) {
+            var c = el.classList;
+            if (c.contains('pictureservices') || c.contains('videoservices') || c.contains('ogvideo') || c.contains('iframeembed') || c.contains('plink')) {
+                e.stopPropagation();
+                return;
+            }
+        }
+    }, true);
+}());
